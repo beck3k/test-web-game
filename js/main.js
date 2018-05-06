589e0cdb743dd2115c48fff82563b4000a12ec16
Beck <physicalc123@gmail.com>
2018-05-05 20:16:08 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -6,18 +6,22 @@ $(window).on('DOMContentLoaded', () => {
   var createScene = function(){
     var scene = new BABYLON.Scene(engine);
 
-    scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.OimoJSPlugin());
-    scene.gravity = new BABYLON.Vector3(0, -2, 0);
+    scene.enablePhysics(new BABYLON.Vector3(0, -9.81, 0), new BABYLON.CannonJSPlugin());
+    scene.gravity = new BABYLON.Vector3(0, -0.25, 0);
 
 
     var camera = new BABYLON.FreeCamera("camera0", new BABYLON.Vector3(0, 50, -10), scene);
-    var ground = BABYLON.Mesh.CreateTiledGround('ground0', -20, -20, 20, 20, {"w" : 2, "h" : 2}, {'h' : 8, 'w' : 8}, scene, true);
+    // var ground = BABYLON.Mesh.CreateTiledGround('ground0', -20, -20, 20, 20, {"w" : 2, "h" : 2}, {'h' : 8, 'w' : 8}, scene, true);
 
     camera.setTarget(BABYLON.Vector3.Zero());
-    camera.ellipsoid = new BABYLON.Vector3(0.1,1,0.1);
+    // camera.ellipsoid = new BABYLON.Vector3(0.1,1,0.1);
     camera.attachControl(canvas, false);
+    camera.keysUp.push(87);
+    camera.keysDown.push(83)
+    camera.keysLeft.push(65);
+    camera.keysRight.push(68);
 
-    var light = new BABYLON.HemisphericLight('light0', new BABYLON.Vector3(0,1,0), scene);
+    var light = new BABYLON.PointLight('light0', new BABYLON.Vector3(0,30,0), scene);
 
     var ground = BABYLON.Mesh.CreateTiledGround('ground0', -20, -20, 20, 20, {"w" : 2, "h" : 2}, {'h' : 8, 'w' : 8}, scene, true);
     ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 0, restitution: 0.9}, scene);
@@ -27,6 +31,65 @@ $(window).on('DOMContentLoaded', () => {
     camera.checkCollisions = true;
 
     ground.checkCollisions = true;
+
+    // var playerBody = new BABYLON.MeshBuilder.CreateBox('playerBody', {size: 2}, scene);
+    // playerBody.physicsImpostor = new BABYLON.PhysicsImpostor(playerBody, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 2, restitution: 0.1}, scene);
+    // camera.parent = playerBody;
+    // camera.position = playerBody.getAbsolutePosition();
+
+    var playCube = new BABYLON.MeshBuilder.CreateBox('playCube', {size: 3, width: 5}, scene);
+    playCube.position = new BABYLON.Vector3(0, 1, 0);
+    playCube.checkCollisions = true;
+
+
+    var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
+
+    // myMaterial.diffuseColor = new BABYLON.Color3(1, 255, 1);
+    // myMaterial.specularColor = new BABYLON.Color3(0.5, 0.6, 0.87);
+    myMaterial.ambientTexture = new BABYLON.Texture('/grass.png', scene);
+    // myMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
+    // myMaterial.ambientColor = new BABYLON.Color3(0.23, 5.98, 0.53);
+
+    playCube.material = myMaterial;
+    playCube.wireframe = true;
+
+    playCube.physicsImpostor = new BABYLON.PhysicsImpostor(playCube, BABYLON.PhysicsImpostor.BoxImpostor, {mass: 2, restitution: 0.1});
+
+
+    var keyHandler = function(evt){
+      switch(evt.key){
+        case " ":
+          var cam = scene.cameras[0];
+
+          cam.animations = [];
+
+          var a = new BABYLON.Animation(
+              "a",
+              "position.y", 20,
+              BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+              BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+          // Animation keys
+          var keys = [];
+          keys.push({ frame: 0, value: cam.position.y });
+          keys.push({ frame: 10, value: cam.position.y + 5 });
+          // keys.push({ frame: 20, value: cam.position.y });
+          a.setKeys(keys);
+
+          var easingFunction = new BABYLON.CircleEase();
+          easingFunction.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT);
+          a.setEasingFunction(easingFunction);
+
+          cam.animations.push(a);
+
+          scene.beginAnimation(cam, 0, 20, false);
+      }
+    }
+
+    BABYLON.Tools.RegisterTopRootEvents([{
+      name: "keydown",
+      handler: keyHandler
+    }]);
 
     scene.onPointerDown = function(e){
       if (!isLocked){
@@ -55,7 +118,7 @@ $(window).on('DOMContentLoaded', () => {
   };
 
   var scene = createScene();
-  // scene.debugLayer.show();
+  scene.debugLayer.show();
   engine.runRenderLoop(() => {
     scene.render();
   });
